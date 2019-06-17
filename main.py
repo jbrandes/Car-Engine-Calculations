20d5fe8113deb4317c5c4d3d4ed1bda84c724872
Jordan Brandes <jordanbrandes@yahoo.com>
2019-06-17 11:41:18 -0500

    Update main.py
    Added more variables and clarified some of the units

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 class Engine:
     
-    def __init__(self, torque, rpm, horsepower, cylinders, displacement, bore, stroke):
+    def __init__(self, torque, rpm, horsepower, cylinders, displacement, bore, stroke, fuel_injector_size):
         
         self.torque = int(torque)
         self.rpm = int(rpm)
@@ -10,16 +10,19 @@ class Engine:
 print("Let's see how your car is running. Tell me about your car.")
 torque = int(input("What is your torque? "))
 rpm = int(input("What is your car's RPM? "))
-cylinders = int(input("how many cylinders do you have?"))
-Bore = int(input("how long is your engine bore measurement?"))
-Stroke = int(input("What is the engine stroke measurement?"))
+cylinders = int(input("how many cylinders do you have? "))
+Bore = int(input("how long is your engine bore measurement? "))
+Stroke = int(input("What is the engine stroke measurement? "))
 horsepower = ((rpm * torque) / 5252)
 displacement = (cylinders * 0.7854 * (Bore**2) * Stroke)
+fuel_injector_size = (horsepower / 16)
+
         
 print("Your horsepower is: ", horsepower) 
-print("Your engine displacement is: ", displacement)
+print("Your engine displacement is: ", displacement, "CCs")
+print("your fuel injector size is: ", fuel_injector_size, "lbs/hr")
     
-if horsepower > 900:
+if horsepower > 300:
     print("I don't know if that's steet legal")
 else:
     print("You should look into a faster car.")
